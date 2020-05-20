5c7cfeb61948a1162933e7375531f140e70ba953
Kaelen Carling <kaelen.carling@gmail.com>
2020-05-20 15:39:02 -0700

    added small rockyou.txt and made zip cracker

--- ViPy Programs/ZipCracker.py
+++ ViPy Programs/ZipCracker.py
@@ -1,9 +1,24 @@
 import zipfile
-zFile = zipfile.ZipFile('C:\\Users\\Kaelen\\PycharmProjects\\ViPy\\ViPy Programs\\resources\\Evil.zip')
-passFile = open('rockyou.txt')
 
-try:
-    #zFile.extractall(pwd=b"crackmes.one")
-    zFile.extractall(pwd=b"password")
-except Exception as e:
-    print(e)
+
+def extractFile(zFile, password):
+    try:
+        zFile.extractall(pwd=password.encode())
+        return password
+    except Exception:
+        return
+
+
+def main():
+    zFile = zipfile.ZipFile('C:\\Users\\Kaelen\\PycharmProjects\\ViPy\\ViPy Programs\\resources\\Evil.zip')
+    passFile = open('rockyou.txt')
+
+    for line in passFile.readlines():
+        password = line.strip('\n')
+        guess = extractFile(zFile, password)
+        if guess:
+            print('Success! password is ' + password)
+
+
+if __name__ == '__main__':
+    main()

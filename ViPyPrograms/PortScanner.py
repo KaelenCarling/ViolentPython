cfb1de3c2f9807921c0982246f933036722ff478
Kaelen Carling <kaelen.carling@gmail.com>
2020-05-26 16:02:50 -0700

    finalized PortScanner.py

--- ViPyPrograms/PortScanner.py
+++ ViPyPrograms/PortScanner.py
@@ -1,4 +1,5 @@
 import optparse
+import socket
 from socket import *
 
 
@@ -6,27 +7,35 @@ def connectionScan(tgtHost, tgtPort):
     try:
         connSkt = socket(AF_INET, SOCK_STREAM)
         connSkt.connect((tgtHost, tgtPort))
+        connSkt.send('ViolentPython\r\n')
+        results = connSkt.recv(100)
+
         print('%d/tcp open' % tgtPort)
+        print(str(results))
+
         connSkt.close()
     except:
         print('%d/tcp closed' % tgtPort)
+
 
 def portScan(tgtHost, tgtPorts):
     try:
         tgtIP = gethostbyname(tgtHost)
     except:
-        print("Cannot resolve '%s': Unkown host"%tgtHost)
+        print("Cannot resolve '%s': Unkown host" % tgtHost)
+        return
 
     try:
         tgtName = gethostbyaddr(tgtIP)
-        print('\n Scan Results for: ' + tgtName[0])
+        print('\nScan Results for: ' + tgtName[0])
     except:
-        print('\n Scan Results for: ' + tgtIP)
+        print('\nScan Results for: ' + tgtIP)
 
     setdefaulttimeout(1)
     for tgtPort in tgtPorts:
         print('Scanning Ports ' + tgtPort)
         connectionScan(tgtHost, int(tgtPort))
+
 
 def main():
     parser = optparse.OptionParser('usage: %prog -H <target host> -p <target port>')
@@ -37,12 +46,13 @@ def main():
     (options, args) = parser.parse_args()
 
     tgtHost = options.tgtHost
-    tgtPort = options.tgtPort
+    tgtPorts = str(options.tgtPort).split(', ')
 
-    if (tgtHost is None) | (tgtPort is None):
-        print(parser.usage)
+    if (tgtHost is None) | (tgtPorts is None):
+        print('Please specify a target host and at least 1 port')
         exit(0)
 
+    portScan(tgtHost, tgtPorts)
 
 if __name__ == '__main__':
     main()

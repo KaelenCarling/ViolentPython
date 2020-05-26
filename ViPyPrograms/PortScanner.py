ab4e224f246ee8609cc7b3e556379230942b9225
Kaelen Carling <kaelen.carling@gmail.com>
2020-05-26 15:38:14 -0700

    added portScan and connectionScan

--- ViPyPrograms/PortScanner.py
+++ ViPyPrograms/PortScanner.py
@@ -1,16 +1,48 @@
 import optparse
+from socket import *
 
-parser = optparse.OptionParser('usage: %prog -H <target host> -p <target port>')
 
-parser.add_option('-H', dest='tgtHost', type='string', help='specify target host')
-parser.add_option('-p', dest='tgtPort', type='int', help='specify target port')
+def connectionScan(tgtHost, tgtPort):
+    try:
+        connSkt = socket(AF_INET, SOCK_STREAM)
+        connSkt.connect((tgtHost, tgtPort))
+        print('%d/tcp open' % tgtPort)
+        connSkt.close()
+    except:
+        print('%d/tcp closed' % tgtPort)
 
-(options, args) = parser.parse_args()
+def portScan(tgtHost, tgtPorts):
+    try:
+        tgtIP = gethostbyname(tgtHost)
+    except:
+        print("Cannot resolve '%s': Unkown host"%tgtHost)
 
-tgtHost = options.tgtHost
-tgtPort = options.tgtPort
+    try:
+        tgtName = gethostbyaddr(tgtIP)
+        print('\n Scan Results for: ' + tgtName[0])
+    except:
+        print('\n Scan Results for: ' + tgtIP)
 
-if (tgtHost is None) | (tgtPort is None):
-    print(parser.usage)
-    exit(0)
+    setdefaulttimeout(1)
+    for tgtPort in tgtPorts:
+        print('Scanning Ports ' + tgtPort)
+        connectionScan(tgtHost, int(tgtPort))
 
+def main():
+    parser = optparse.OptionParser('usage: %prog -H <target host> -p <target port>')
+
+    parser.add_option('-H', dest='tgtHost', type='string', help='specify target host')
+    parser.add_option('-p', dest='tgtPort', type='int', help='specify target port')
+
+    (options, args) = parser.parse_args()
+
+    tgtHost = options.tgtHost
+    tgtPort = options.tgtPort
+
+    if (tgtHost is None) | (tgtPort is None):
+        print(parser.usage)
+        exit(0)
+
+
+if __name__ == '__main__':
+    main()
